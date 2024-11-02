6684bae48890511d8c54e5f91bb06461e40174ea
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-11-02 12:36:37 +0500

    Fixed ESLint errors for deployment

--- 01_countdown_timer/src/components/count-down.tsx
+++ 01_countdown_timer/src/components/count-down.tsx
@@ -1,5 +1,5 @@
 'use client';
-import {useState, useRef, useEffect, ChangeEvent, use} from 'react';
+import {useState, useRef, useEffect, ChangeEvent} from 'react';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 
